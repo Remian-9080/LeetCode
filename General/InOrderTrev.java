dcce12cff9921b21e5490441e3a322c6e0eca461
Remian 9080 <rakibpsp@gmail.com>
2024-02-04 23:41:17 +0600

    New Solved

---
+++ General/InOrderTrev.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+public class InOrderTrev {
+  
+    private List<Integer> res = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        traverse(root);
+        return res;
+    }
+    
+    private void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        traverse(root.left);
+        res.add(root.val);
+        traverse(root.right);
+    }
+}
+
