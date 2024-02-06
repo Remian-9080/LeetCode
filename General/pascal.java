eb8c123232252b4f4d501b428dc62541fcfacc6f
Remian 9080 <rakibpsp@gmail.com>
2024-02-06 23:55:20 +0600

    New Solved

--- General/pascal.java
+++ General/pascal.java
@@ -1,9 +1,10 @@
 import java.util.List;
-
+/* Learn about linked list */
 public class pascal 
 {
   public List<List<Integer>> generate(int numRows)
-   {
-        kjklklnk
+    {
+
+    
     }    
 }
